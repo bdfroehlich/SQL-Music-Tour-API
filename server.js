9f486705a614c1df8180e1c708f66c25ca6a5fee
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-04-20 20:56:06 -0500

    commit activity

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+require('dotenv').config();
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+// // const sequelize = new Sequelize(process.env.PG_URI)
+// const sequelize = new Sequelize({password:"benj4min21", username:"postgres", host: "localhost", port: 5432, database:"music_tour", dialect:"postgres"})
+
+// try {
+//     sequelize.authenticate() 
+//     console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+// } catch(err) {
+//     console.log(`Unable to connect to PG: ${err}`) 
+// }
+
 
 // ROOT
 app.get('/', (req, res) => {
