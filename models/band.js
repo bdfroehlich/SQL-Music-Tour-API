7fcbad913c54c44ea3208dc6a8530331dcf24b1e
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-04-25 19:31:08 -0500

    adding associations

--- models/band.js
+++ models/band.js
@@ -9,7 +9,19 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({Meet_Greet, Set_Time}) {
+      //meet and greets
+      Band.hasMany(Meet_Greet, {
+        //foreign key on the Meet_Greet table
+        foreignKey: "band_id",
+        as: "meet_greets"
+      })
+
+      //set times
+      Band.hasMany(Set_Time, {
+        foreignKey: "band_id",
+        as: "set_times"
+      })
     }
   }
   Band.init({
